f761181142e0044fcea37757dbc3c0a785522928
Artem Gandybin <arteg@mail.ru>
2019-05-14 11:01:06 +0300

    code format

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -10,7 +10,7 @@ package ru.job4j.calculate;
 
 public class Calculate {
 
- /**
+    /**
      * Main.
      * @param args - args.
      */
@@ -18,12 +18,12 @@ public class Calculate {
         System.out.println("Hello World");
     }
 
-/**
- * Method echo.
- * @param name Your name.
- * @return Echo plus your name.
- */
- public String echo(String name) {
-    return "Echo, echo, echo : " + name;
- }
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
